c6c3fa697c0ac00a03bf7f8967d144c6e01468fc
iRev Root <root@irev.host>
2019-01-30 14:33:53 -0800

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Presence's iRev Firewall Generator
+Use my abuseipdb API key and the general spamhaus shitlist to keep iRev Seattle safer
+FreeBSD & IPFW
+20190130
---
+++ abuseipdb.pl
@@ -0,0 +1,48 @@
+#!/usr/local/bin/perl
+## Presence's iRev Firewall Generator
+## Use my abuseipdb API key and the general spamhaus shitlist to keep iRev Seattle safer
+## 20190130
+use strict;
+use warnings;
+use POSIX qw/strftime/;
+
+our $key;
+require "/root/firewall/api-key.pl"; # which is just:  our $key="asdf";
+
+my $today = strftime('%Y%m%d',localtime);
+
+my $list = `curl -s -G https://api.abuseipdb.com/api/v2/blacklist -d countMinimum=15 -d maxAgeInDays=60 -d confidenceMinimum=90 -d plaintext -H "Key: $key" -H "Accept: text/plain"`;
+open(my $fh, '>', "/root/firewall/firewall-blacklist-$today.txt");
+my @lines = split /\n/, $list;
+foreach my $line (@lines) {
+	chomp $line;
+	$line =~ s/;.*//;
+	next if (length($line) < 4);
+	if ($line !~ m/\/\d/) {
+		$line =~ s/$/\/32/;
+	}
+	print ($fh "$line\n");
+}
+close $fh;
+
+$list = `curl -s http://www.spamhaus.org/drop/drop.lasso`;
+open($fh, '>>', "/root/firewall/firewall-blacklist-$today.txt");
+@lines = split /\n/, $list;
+foreach my $line (@lines) {
+	chomp $line;
+	$line =~ s/;.*//;
+	next if (length($line) < 4);
+	if ($line !~ m/\/\d/) {
+		$line =~ s/$/\/32/;
+	}
+	print ($fh "$line\n");
+}
+close $fh;
+
+`ipfw table 2 flush`;
+open($fh, '<', "/root/firewall/firewall-blacklist-$today.txt");
+while (my $ip = <$fh>) {
+	chomp $ip;
+	`ipfw table 2 add $ip`;
+}
+close $fh;
