817fac2aa37386295b698468a8d5a0bff2859b98
Presence <presence@irev.net>
2019-02-08 11:53:54 -0800

    Added Raspberry Pi ipchains gizmo, removed \s+$

--- abuseipdb-ipchains.pl
+++ abuseipdb-ipchains.pl
@@ -1,18 +1,25 @@
-#!/usr/local/bin/perl
-## Presence's iRev Firewall Generator
-## Use my abuseipdb API key and the general spamhaus shitlist to keep iRev Seattle safer
-## 20190130
+#!/usr/bin/perl
+## Presence's Raspberry Pi Firewall Generator
+## Use my abuseipdb API key and the general spamhaus shitlist to keep my RPi safer
+## 20190208
+##  notes:
+##   list the ipset group in iptables:
+## iptables --line-numbers --list PREROUTING -t raw
+##   delete the ipset group:
+## iptables -t raw -D PREROUTING 1
+##   flush the ipset entirely:
+## ipset x
 use strict;
 use warnings;
 use POSIX qw/strftime/;
 
 our $key;
-require "/root/firewall/api-key.pl"; # which is just:  our $key="asdf";
+require "/root/ipfw-firewall/api-key.pl"; # which is just:  our $key="asdf";
 
 my $today = strftime('%Y%m%d',localtime);
 
 my $list = `curl -s -G https://api.abuseipdb.com/api/v2/blacklist -d countMinimum=15 -d maxAgeInDays=60 -d confidenceMinimum=90 -d plaintext -H "Key: $key" -H "Accept: text/plain"`;
-open(my $fh, '>', "/root/firewall/firewall-blacklist-$today.txt");
+open(my $fh, '>', "/root/ipfw-firewall/firewall-blacklist-$today.txt");
 my @lines = split /\n/, $list;
 foreach my $line (@lines) {
 	chomp $line;
@@ -27,7 +34,7 @@ foreach my $line (@lines) {
 close $fh;
 
 $list = `curl -s http://www.spamhaus.org/drop/drop.lasso`;
-open($fh, '>>', "/root/firewall/firewall-blacklist-$today.txt");
+open($fh, '>>', "/root/ipfw-firewall/firewall-blacklist-$today.txt");
 @lines = split /\n/, $list;
 foreach my $line (@lines) {
 	chomp $line;
@@ -41,14 +48,17 @@ foreach my $line (@lines) {
 }
 close $fh;
 
-`ipfw table 2 flush`;
-open($fh, '<', "/root/firewall/firewall-blacklist-$today.txt");
+`ipset -exist -N badips hash:net`;
+`ipset flush badips`;
+open($fh, '<', "/root/ipfw-firewall/firewall-blacklist-$today.txt");
 while (my $ip = <$fh>) {
 	chomp $ip;
 	if ($ip =~ /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2})/) {
-		`ipfw table 2 add $ip`;
+		`ipset -A badips "$ip"`;
 	} else {
 		print "Some weird stuff in the firewall list $today: $ip\n";
 	}
 }
 close $fh;
+`iptables -t raw -I PREROUTING -m set --match-set badips src,dst -j DROP`;
+
